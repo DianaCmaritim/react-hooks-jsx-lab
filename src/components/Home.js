71b3f6d2130762454d71e18c1988117d9359c95c
Diana <maritimdiane96@gmail.com>
2023-01-09 22:19:03 +0300

    components,parent and child elements

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,15 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return(
+  <div id="home">
+    <h1 style={{color: "firebrick"}}>Diana is a Web Developer from Wakanda</h1>
+    <div id="home">
+      <h1>
+        {name} is a Web Developer from {city}
+      </h1>
+    </div>
+  </div>);
 }
 
 export default Home;
